8732e720cd45e32f7a003ceee87ab3168c2e03fb
FrankvandenBoogaard <frank@exprimo.nl>
2022-05-04 00:24:09 +0200

    feat/challenge 2

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './styles.css';
 
 function NavBar() {
   return (
     <div className='container'>
       <div className='links-container'>
-        <Link to='/'>Home</Link> | <Link to='/shop'>Shop</Link> |{' '}
-        <Link to='/details/1'>About</Link>
+        <NavLink
+          className='links'
+          to='/'
+          style={({ isActive }) => (isActive ? { color: 'white' } : undefined)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className='links'
+          to='/shop'
+          style={({ isActive }) => (isActive ? { color: 'white' } : undefined)}
+        >
+          Shop
+        </NavLink>
+
+        <NavLink
+          className='links'
+          to='/details/1'
+          style={({ isActive }) => (isActive ? { color: 'white' } : undefined)}
+        >
+          About
+        </NavLink>
       </div>
       <div className='icons-container'></div>
     </div>
