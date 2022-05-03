d5bbc3f94a6835cbf959e660298c3ddb9cc20b0a
FrankvandenBoogaard <frank@exprimo.nl>
2022-05-03 21:14:27 +0200

    feat/built_skeleton

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from "./NavBar";
+//export * from "./Button";
+//export * from "./OtherComponent";
+
+// use as:  import { NavBar, Button, ProductCard } from "../components"; // Much Cleaner!! :D
